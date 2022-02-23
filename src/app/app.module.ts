21540a9ba268cac4dfb35122faa5e728a81e4d7b
Oumaima Khadri <oumaimakh@pre-history.com>
2022-02-23 01:55:35 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FooterComponent } from './components/footer/footer.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+
+const appRoutes: Routes = [
+  { path: '', component: TasksComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent,
+    FooterComponent,
+    AddTaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
   ],
   providers: [],
   bootstrap: [AppComponent]
